0163538427b964aa3f5bfc65ffa20116971b1956
24LP4300 24LP4300 <24LP4300@G305-CC0063.local>
2024-09-10 13:49:19 +0800

    safelist

--- tailwind.config.js
+++ tailwind.config.js
@@ -15,4 +15,16 @@ module.exports = {
     },
   },
   plugins: [require("@tailwindcss/typography"), require("daisyui")],
+
+  safelist: [
+    "flex",
+    "flex-wrap",
+    "flex-col ",
+    "justify-center",
+    "items-center",
+    " grid ",
+    "grid-cols-3",
+    "justify-end",
+    "items-end",
+  ],
 };
